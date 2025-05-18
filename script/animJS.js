b4fa2ef186949d0428a3d77cdab9fc86ea4230f5
Knoppiix <gwendal.p@protonmail.com>
2025-05-18 02:28:26 -0400

    lightbulb animation added

---
+++ script/animJS.js
@@ -0,0 +1,67 @@
+import { animate, utils, createSpring, createTimeline } from 'animejs';
+
+const [ $frame ] = utils.$('.frame');
+const [ $frameText ] = utils.$('.frameText');
+
+const frameText = "Animation experiments";
+let randomDelay = utils.random(0, 600);
+
+for (let i = 0; i < frameText.length; i++) {
+    const node = document.createElement('span');
+    node.innerHTML = frameText[i] === ' ' ? '&nbsp;' : frameText[i];
+    $frameText.appendChild(node);
+}
+
+const frameAnimation = animate($frame,{  
+    transform: ['scaleX(0)', 'scaleX(1)'],
+    ease: 'cubic-bezier(0.25,-0.03, 0.82, 0.83)', 
+    duration: 200,        
+});
+
+
+const frameAnimationText = animate($frameText.children,{
+    opacity: [0,1],
+    ease: 'easeOutQuad',    
+    delay: (_, i) => i * 20,
+});
+
+const spark1Appear = animate("#spark1", {
+  scale: [0, 1, 0],
+  ease: 'steps(2)',
+  duration: 600,
+  loopDelay: 8000,      
+  loop: true,
+  alternate: true,
+});
+
+const spark2Appear = animate("#spark2", {
+  scale: [0, 1, 0],
+  ease: 'steps(2)',
+  duration: 600,
+  delay: () => randomDelay,
+  loopDelay: 8000,      
+  loop: true,
+  alternate: true,
+  onLoopComplete: () => {
+    randomDelay = utils.random(0, 600);
+  }
+});
+
+const lightBulbAppear = animate(".string", {
+  height: ["0", "5rem"],
+  ease: createSpring({stiffness: 100, damping: 10}),
+  duration: 200,
+});
+
+const frameTimeline = createTimeline()
+.add(frameAnimation)
+.add(frameAnimationText);
+
+
+const sparkTimeline = createTimeline()
+.add(spark1Appear)
+.add(spark2Appear);
+
+const timeline = createTimeline()
+.sync(frameTimeline)
+.sync(sparkTimeline);
