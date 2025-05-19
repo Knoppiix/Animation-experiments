6ebd162920336ada9d43d82986126025a136aa6b
Knoppiix <gwendal.p@protonmail.com>
2025-05-19 01:49:32 -0400

    added easter egg

--- script/animJS.js
+++ script/animJS.js
@@ -4,13 +4,58 @@ const [ $frame ] = utils.$('.frame');
 const [ $frameText ] = utils.$('.frameText');
 
 const frameText = "Animation experiments";
+const [ hands ] = utils.$('.hands');
+const [ container ] = utils.$('.container');
 let randomDelay = utils.random(0, 600);
+const somethingAudio = new Audio('assets/something.mp3');
+
+async function isAudioUnlocked(audioElement) {
+  // Try to play the audio (muted to avoid sound)
+  const prevMuted = audioElement.muted;
+  audioElement.muted = true;
+  try {
+      await audioElement.play();
+      audioElement.pause(); // Stop playback if it worked
+      audioElement.muted = prevMuted;
+      return true;
+  } catch (e) {
+      audioElement.muted = prevMuted;
+      return false;
+  }
+}
+
+const playAudio = () => {
+  isAudioUnlocked(somethingAudio).then(unlocked => {
+    if (unlocked) {
+        somethingAudio.play();
+        animate(".hands", {
+          translateY: ["0", "85%"],
+          duration: 1000,
+          ease: "easeOutQuad",
+          alternate: true,
+          loopDelay: 1000,
+          loop: 1,
+        })
+        container.removeEventListener('mouseover', playAudio)
+}
+  }, { once: true })
+}
+container.addEventListener('mouseover', playAudio)
+
 
 for (let i = 0; i < frameText.length; i++) {
     const node = document.createElement('span');
     node.innerHTML = frameText[i] === ' ' ? '&nbsp;' : frameText[i];
     $frameText.appendChild(node);
 }
+
+
+for(let i = 0; i < 10; i++){
+    const newHand = hands.cloneNode(true);
+    newHand.style.rotate = `${i * 36}deg`;
+    container.appendChild(newHand);
+}
+
 
 const frameAnimation = animate($frame,{  
     transform: ['scaleX(0)', 'scaleX(1)'],
@@ -52,6 +97,16 @@ const lightBulbAppear = animate(".string", {
   ease: createSpring({stiffness: 100, damping: 10}),
   duration: 200,
 });
+
+animate(['feTurbulence', 'feDisplacementMap'], {
+  baseFrequency: 0.03,
+  scale: 40,
+  ease: 'linear',
+  alternate: true,
+  duration: 1500,
+  loop: true
+});
+
 
 const frameTimeline = createTimeline()
 .add(frameAnimation)
